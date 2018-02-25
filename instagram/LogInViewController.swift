da384ec923dd67c22295955400d0f20d89b1f546
johnjing <838633350@qq.com>
2018-02-24 21:12:02 -0800

    First commit, app connected to MongoDB

---
+++ instagram/LogInViewController.swift
@@ -0,0 +1,53 @@
+//
+//  LogInViewController.swift
+//  instagram
+//
+//  Created by JY on 2018/2/21.
+//  Copyright © 2018年 JY. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LogInViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        newUser.signUpInBackground { (success: Bool, Error: Error?) in
+            if success {
+                print("Yay, created!")
+            }
+            else {
+                print(Error!.localizedDescription)
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
